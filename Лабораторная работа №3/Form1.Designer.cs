e03ff53d7b663d6133058acc4694d1b0877f50be
Dmitry Lvov <voz-demon.ru@mail.ru>
2021-04-18 21:56:53 +0300

    Внешний вид + кнопки

--- Лабораторная работа №3/Form1.Designer.cs
+++ Лабораторная работа №3/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menu = new System.Windows.Forms.MenuStrip();
             this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -39,7 +40,11 @@
             this.highScoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mineandtimepanel = new System.Windows.Forms.TableLayoutPanel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labeltimer = new System.Windows.Forms.Label();
+            this.labelmine = new System.Windows.Forms.Label();
             this.menu.SuspendLayout();
+            this.mineandtimepanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // menu
@@ -66,13 +71,13 @@
             this.highScoresToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
-            this.gameToolStripMenuItem.Size = new System.Drawing.Size(74, 32);
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(74, 30);
             this.gameToolStripMenuItem.Text = "Game";
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 32);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 30);
             this.aboutToolStripMenuItem.Text = "About";
             // 
             // newGameToolStripMenuItem
@@ -119,17 +124,53 @@
             // 
             // mineandtimepanel
             // 
-            this.mineandtimepanel.ColumnCount = 2;
+            this.mineandtimepanel.ColumnCount = 5;
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
             this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
             this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mineandtimepanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mineandtimepanel.Controls.Add(this.labeltimer, 3, 0);
+            this.mineandtimepanel.Controls.Add(this.labelmine, 1, 0);
             this.mineandtimepanel.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.mineandtimepanel.Location = new System.Drawing.Point(0, 350);
             this.mineandtimepanel.Name = "mineandtimepanel";
-            this.mineandtimepanel.RowCount = 2;
-            this.mineandtimepanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.mineandtimepanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mineandtimepanel.RowCount = 1;
+            this.mineandtimepanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.mineandtimepanel.Size = new System.Drawing.Size(800, 100);
             this.mineandtimepanel.TabIndex = 1;
+            // 
+            // labeltimer
+            // 
+            this.labeltimer.AutoSize = true;
+            this.labeltimer.BackColor = System.Drawing.SystemColors.Window;
+            this.labeltimer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labeltimer.Font = new System.Drawing.Font("Consolas", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labeltimer.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labeltimer.Location = new System.Drawing.Point(430, 0);
+            this.labeltimer.Name = "labeltimer";
+            this.labeltimer.Size = new System.Drawing.Size(349, 100);
+            this.labeltimer.TabIndex = 0;
+            this.labeltimer.Text = "labeltimer";
+            this.labeltimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelmine
+            // 
+            this.labelmine.AutoSize = true;
+            this.labelmine.BackColor = System.Drawing.SystemColors.Window;
+            this.labelmine.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelmine.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelmine.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelmine.Location = new System.Drawing.Point(39, 0);
+            this.labelmine.Name = "labelmine";
+            this.labelmine.Size = new System.Drawing.Size(349, 100);
+            this.labelmine.TabIndex = 1;
+            this.labelmine.Text = "labelmine";
+            this.labelmine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Minesweeper
             // 
@@ -143,6 +184,8 @@
             this.Text = "Minesweeper";
             this.menu.ResumeLayout(false);
             this.menu.PerformLayout();
+            this.mineandtimepanel.ResumeLayout(false);
+            this.mineandtimepanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +204,9 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.TableLayoutPanel mineandtimepanel;
+        private System.Windows.Forms.Label labeltimer;
+        private System.Windows.Forms.Label labelmine;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
